fa3ae1f11e9e2e76350e0c36fd949bf9479096ef
Vimal Hirapara <Vimal.Hirapara@contis.com>
2023-03-07 14:33:39 +0530

    add *testing.M test case

---
+++ test1/main_testing_m_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+//	go test -v .\main_testing_m_test.go
+
+func TestMain(m *testing.M) {
+	log.Println("Do stuff BEFORE the tests!")
+	exitVal := m.Run()
+	log.Println("Do stuff AFTER the tests!")
+
+	os.Exit(exitVal)
+}
+
+func TestA(t *testing.T) {
+	log.Println("TestA running")
+}
+
+func TestB(t *testing.T) {
+	log.Println("TestB running")
+}
